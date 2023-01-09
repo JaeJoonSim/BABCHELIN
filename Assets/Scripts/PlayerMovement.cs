c83f9da17c2fad4fbd20265add94253a233ed2f6
JaeJoonSim <104071482+JaeJoonSim@users.noreply.github.com>
2023-01-09 16:34:59 +0900

    Basic Player Move, Cinemachine Camera
    플레이어 이동, 점프 (수정필요)
    시네머신 카메라(플레이어 따라가기)

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,102 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerMovement : MonoBehaviour
+{
+    #region Inspector Fields
+    [SerializeField]
+    [Tooltip("PC 걷기의 최대 이동 속도")]
+    private float pcWalkMSpd;
+    public float PcWalkMSpd
+    {
+        get { return pcWalkMSpd; }
+        set { pcWalkMSpd = value; }
+    }
+
+    [SerializeField]
+    [Tooltip("PC 뛰기의 최대 이동 속도")]
+    private float pcRunMSpd;
+    public float PcRunMSpd
+    {
+        get { return pcRunMSpd; }
+        set { pcRunMSpd = value; }
+    }
+
+    [SerializeField]
+    [Tooltip("PC 점프 속도")]
+    private float pcJumpHeight;
+    public float PcJumpHeight
+    {
+        get { return pcJumpHeight; }
+        set { pcJumpHeight = value; }
+    }
+
+    [SerializeField]
+    [Tooltip("PC 점프 쿨타임")]
+    private float pcJumpDelay;
+    public float PcJumpDelay
+    {
+        get { return pcJumpDelay; }
+        set { pcJumpDelay = value; }
+    }
+
+    /// <summary>
+    /// True = Walk, False = Run
+    /// </summary>
+    [SerializeField]
+    [Tooltip("걷기/뛰기 스탠스 트리거")]
+    private bool pcWRTrigger;
+    public bool PcWRTrigger
+    {
+        get { return pcWRTrigger; }
+        set { pcWRTrigger = value; }
+    }
+
+    #endregion
+
+    #region Private Value
+    private Rigidbody rb;
+
+    private float groundDistance = .1f;
+
+    #region Player Input Fields
+    private float inputX;
+    private float inputZ;
+    #endregion
+
+    #endregion
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        PlayerMove();
+
+        if (Input.GetButtonDown("Jump") && IsGrounded())
+        {
+            rb.AddForce(new Vector3(0, pcJumpHeight, 0), ForceMode.Impulse);
+        }
+    }
+
+    private void PlayerMove()
+    {
+        inputX = Input.GetAxis("Horizontal");
+        inputZ = Input.GetAxis("Vertical");
+
+        Vector3 vel = new Vector3(inputX, 0, inputZ);
+        
+        vel = (pcWRTrigger) ? vel *= pcWalkMSpd : vel *= pcRunMSpd;
+
+        rb.velocity = vel;
+    }
+
+    private bool IsGrounded()
+    {
+        return Physics.Raycast(transform.position, Vector3.down, groundDistance);
+    }
+}
