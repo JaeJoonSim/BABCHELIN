631329e6006d29358a8de98bd25789ca11f0e37c
JaeJoonSim <104071482+JaeJoonSim@users.noreply.github.com>
2023-01-13 01:55:09 +0900

    Inventory System - Tester Panel

--- Assets/Scripts/InventorySystem/InventoryTester.cs
+++ Assets/Scripts/InventorySystem/InventoryTester.cs
@@ -5,6 +5,8 @@ using UnityEngine.UI;
 
 public class InventoryTester : MonoBehaviour
 {
+    public bool showTesterPanel;
+    public GameObject testPanel;
     public Inventory inventory;
     public ItemData[] itemDataArray;
 
@@ -14,5 +16,17 @@ public class InventoryTester : MonoBehaviour
     private void Start()
     {
         testButton1.onClick.AddListener(() => { inventory.Add(itemDataArray[0]); });
+    }
+
+    private void Update()
+    {
+        if (showTesterPanel)
+        {
+            testPanel.SetActive(true);
+        }
+        else
+        {
+            testPanel.SetActive(false);
+        }
     }
 }
