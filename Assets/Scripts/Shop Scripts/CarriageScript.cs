a2bf70c7cdbb94080983bb13e651b722b23266bd
kdh990415 <kimdae990415@gmail.com>
2023-03-24 18:52:18 +0900

    마차 UI 재료보관함 + 테이블 구현

--- Assets/Scripts/Shop Scripts/CarriageScript.cs
+++ Assets/Scripts/Shop Scripts/CarriageScript.cs
@@ -7,15 +7,28 @@ public class CarriageScript : MonoBehaviour
     public GameObject Manager;
     TimeScript timeScript;
 
+    [Tooltip("요리작업대")]
+    [SerializeField]
+    private Inventory cook;
+    public Inventory Cook { get { return cook; } }
+
     private void Start()
     {
+
         timeScript = Manager.GetComponent<TimeScript>();
     }
     public void OpenCarriage()
     {
         if(timeScript.isNight == true)
         {
-            Debug.Log("마차 오픈");
+            if (cook.Items.Items[0].Item.ID == -1 || cook.Items.Items[1].Item.ID == -1 || cook.Items.Items[2].Item.ID == -1 || cook.Items.Items[3].Item.ID == -1)
+            {
+                Debug.Log("요리 재료 부족");
+            }
+            else
+            {
+                Debug.Log("마차 오픈");
+            }
         }
         else
         {
