98ccdfca9c86360ecdb45dae53b873e543f2acec
JaeJoonSim <104071482+JaeJoonSim@users.noreply.github.com>
2023-03-24 16:52:23 +0900

    Basic Gird Placement System

---
+++ Assets/Scripts/Grid Placement System/DragAndDrop.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragAndDrop : MonoBehaviour
+{
+    public LayerMask mask;
+    public int LastPosX, LastPosZ;
+    public float LastPosY;
+    private Vector3 mousepos;
+
+    private GameObject plain;
+    private Renderer rend;
+    public Material matGrid, matDefault;
+    private bool isdraging = false;
+
+
+    void Start()
+    {
+        plain = GameObject.Find("Ground");
+        rend = plain.GetComponent<Renderer>();
+
+    }
+
+    private void OnMouseDown()
+    {
+        isdraging = true;
+    }
+
+    private void OnMouseUp()
+    {
+        isdraging = false;
+    }
+
+    private void OnMouseDrag()
+    {
+        if (isdraging)
+        {
+            mousepos = Input.mousePosition;
+            Ray ray = Camera.main.ScreenPointToRay(mousepos);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity, mask))
+            {
+                //if (hit.collider.gameObject == gameObject)
+                {
+                    int PosX = (int)Mathf.Round(hit.point.x);
+                    int PosZ = (int)Mathf.Round(hit.point.z);
+                    if (PosX != LastPosX || PosZ != LastPosZ)
+                    {
+                        LastPosX = PosX;
+                        LastPosZ = PosZ;
+                        transform.position = new Vector3(PosX, LastPosY, PosZ);
+                    }
+                }
+            }
+        }
+    }
+
+    void Update()
+    {
+        if (isdraging)
+        {
+            rend.material = matGrid;
+        }
+        else if (!isdraging)
+        {
+            rend.material = matDefault;
+        }
+    }
+}
