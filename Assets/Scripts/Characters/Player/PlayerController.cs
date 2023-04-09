ba97f4525b767dc285db550f026231a2eb8f217d
JaeJoonSim <104071482+JaeJoonSim@users.noreply.github.com>
2023-04-09 23:30:52 +0900

    Player Move

--- Assets/Scripts/Characters/Player/PlayerController.cs
+++ Assets/Scripts/Characters/Player/PlayerController.cs
@@ -1,15 +1,18 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : BaseMonoBehaviour
 {
+    [HideInInspector] UnitObject unitObject;
     private StateMachine state;
     private CircleCollider2D circleCollider2D;
 
     public Transform SpineTransform;
 
     [HideInInspector] public float defaultRunSpeed = 5.5f;
+    public float runSpeed = 5.5f;
     public static float MinInputForMovement = 0.3f;
 
     public float forceDir;
@@ -22,8 +25,10 @@ public class PlayerController : BaseMonoBehaviour
 
     private void Start()
     {
+        unitObject = base.gameObject.GetComponent<UnitObject>();
         state = base.gameObject.GetComponent<StateMachine>();
         circleCollider2D = base.gameObject.GetComponent<CircleCollider2D>();
+        defaultRunSpeed = runSpeed;
     }
 
     private void Update()
@@ -33,24 +38,44 @@ public class PlayerController : BaseMonoBehaviour
             return;
         }
 
+        xDir = Input.GetAxis("Horizontal");
+        yDir = Input.GetAxis("Vertical");
+        if(state.CURRENT_STATE == StateMachine.State.Moving)
+        {
+            speed *= Mathf.Clamp01(new Vector2(xDir, yDir).magnitude);
+        }
+        speed = Mathf.Max(speed, 0f);
+        unitObject.vx = speed * Mathf.Cos(forceDir * ((float)Math.PI / 180f));
+        unitObject.vy = speed * Mathf.Sin(forceDir * ((float)Math.PI / 180f));
+
         switch (state.CURRENT_STATE)
         {
             case StateMachine.State.Idle:
                 Z = 0f;
                 SpineTransform.localPosition = Vector3.zero;
                 speed += (0f - speed) / 3f * GameManager.DeltaTime;
-                if(Mathf.Abs(xDir) > MinInputForMovement || Mathf.Abs(yDir) > MinInputForMovement)
+                if (Mathf.Abs(xDir) > MinInputForMovement || Mathf.Abs(yDir) > MinInputForMovement)
                 {
                     state.CURRENT_STATE = StateMachine.State.Moving;
                 }
                 break;
             case StateMachine.State.Moving:
-                if(Time.timeScale == 0f)
+                if (Time.timeScale == 0f)
                 {
                     break;
                 }
+                if (Mathf.Abs(xDir) < MinInputForMovement && Mathf.Abs(yDir) < MinInputForMovement)
+                {
+                    state.CURRENT_STATE = StateMachine.State.Idle;
+                    break;
+                }
                 forceDir = Utils.GetAngle(Vector3.zero, new Vector3(xDir, yDir));
-
+                if(unitObject.vx != 0f || unitObject.vy != 0f)
+                {
+                    state.facingAngle = Utils.GetAngle(base.transform.position, base.transform.position + new Vector3(unitObject.vx, unitObject.vy));
+                }
+                state.LookAngle = state.facingAngle;
+                speed += (runSpeed - speed) / 3f * GameManager.DeltaTime;
                 break;
         }
 
---
+++ Assets/Scripts/Characters/SimpleSpineAnimator.cs
@@ -0,0 +1,33 @@
+using Spine.Unity;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleSpineAnimator : BaseMonoBehaviour
+{
+    public AnimationReferenceAsset Idle;
+
+    public AnimationReferenceAsset IdleWithItem;
+
+    public AnimationReferenceAsset MovingWithItem;
+
+    public AnimationReferenceAsset Moving;
+
+    public AnimationReferenceAsset Inactive;
+
+    public AnimationReferenceAsset Action;
+
+    public AnimationReferenceAsset Sleeping;
+
+    public AnimationReferenceAsset PreAttack;
+
+    public AnimationReferenceAsset PostAttack;
+
+    public AnimationReferenceAsset Defending;
+
+    public AnimationReferenceAsset HitLeft;
+
+    public AnimationReferenceAsset HitRight;
+
+    public AnimationReferenceAsset Dodge;
+}
