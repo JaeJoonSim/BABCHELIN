4652d8fb897034aa54b632ea2715d9615d35f49e
kdh990415 <kimdae990415@gmail.com>
2023-04-21 01:40:37 +0900

    cookie mouse 구현중

--- Assets/Scripts/Characters/Enemy/CookieMouse2.cs
+++ Assets/Scripts/Characters/Enemy/CookieMouse2.cs
@@ -3,8 +3,9 @@ using UnityEngine;
 using UnityEngine.AI;
 using Spine;
 using Spine.Unity;
+using System.Collections;
 
-public class CookieMouse : UnitObject
+public class CookieMouse2 : UnitObject
 {
     public Transform SpineTransform;
 
@@ -15,18 +16,20 @@ public class CookieMouse : UnitObject
 
     [SerializeField] Transform target;
     [SerializeField] float detectionRange;
-    [SerializeField] float AttackDistance = 1f;
+    [SerializeField] float attackDetectionRange;
+    [SerializeField] float AttackDistance = 0f;
 
     [Space]
     [SerializeField] float AttackDelay;
     public float AttackDuration = 0.3f;
     [SerializeField] float AttackTimer;
-    [SerializeField] float stopTimer;
+    [SerializeField] float moveSpeed;
 
     private Health playerHealth;
     private NavMeshAgent agent;
     private bool isPlayerInRange;
     private float distanceToPlayer;
+    Vector3 movePoint;
 
     [Space]
 
@@ -36,8 +39,11 @@ public class CookieMouse : UnitObject
 
     private SkeletonAnimation spineAnimation;
 
+    [SerializeField] GameObject slowObj;
+
     private void Start()
     {
+        movePoint = target.position;
         if (target == null)
         {
             target = GameObject.FindGameObjectWithTag("Player").transform;
@@ -57,11 +63,6 @@ public class CookieMouse : UnitObject
     {
         base.Update();
 
-        if (state.CURRENT_STATE != StateMachine.State.Dead)
-        {
-            FollowTarget();
-        }
-
         if (state.CURRENT_STATE == StateMachine.State.Moving)
         {
             speed *= Mathf.Clamp(new Vector2(xDir, yDir).magnitude, 0f, 3f);
@@ -72,123 +73,78 @@ public class CookieMouse : UnitObject
 
         if (state.CURRENT_STATE != StateMachine.State.Dead)
         {
+            Vector3 directionToTarget;
+            float distanceToPoint;
             switch (state.CURRENT_STATE)
             {
                 case StateMachine.State.Idle:
-                    if (isPlayerInRange)
+                    distanceToPlayer = Vector3.Distance(transform.position, target.position);
+                    if (distanceToPlayer <= detectionRange)
                     {
+                        Debug.Log("pc 감지");
                         state.facingAngle = Utils.GetAngle(base.transform.position, base.transform.position + new Vector3(vx, vy));
                         state.LookAngle = state.facingAngle;
-                        stopTimer += Time.deltaTime;
-                    }
-                    else
-                    {
-                        stopTimer = 0f;
-                    }
-
-                    if(stopTimer >= 1)
                         state.CURRENT_STATE = StateMachine.State.Moving;
+                    }
+                    agent.isStopped = true;
 
                     SpineTransform.localPosition = Vector3.zero;
-                    //speed += (0f - speed) / 3f * GameManager.DeltaTime;
+                    speed += (0f - speed) / 3f * GameManager.DeltaTime;
                     break;
                 case StateMachine.State.Moving:
-                    agent.speed = 3f;
-                    AttackTimer = 0f;
-                    if (Time.timeScale == 0f)
-                    {
-                        break;
-                    }
-                    forceDir = Utils.GetAngle(Vector3.zero, new Vector3(xDir, yDir));
+                    agent.speed = moveSpeed;
 
+                    directionToTarget = (target.position - transform.position).normalized;
+
+                    xDir = Mathf.Clamp(directionToTarget.x, -1f, 1f);
+                    yDir = Mathf.Clamp(directionToTarget.y, -1f, 1f);
+
+                    agent.SetDestination(target.position);
+                    agent.isStopped = false;
+
+                    distanceToPoint = Vector3.Distance(transform.position, target.position);
+                    if (distanceToPoint <= attackDetectionRange)
+                    {
+                        Debug.Log("pc 공격");
+                        movePoint = target.position;
+                        state.CURRENT_STATE = StateMachine.State.Attacking;
+                    }
+
+                    forceDir = Utils.GetAngle(Vector3.zero, new Vector3(xDir, yDir));
                     state.facingAngle = Utils.GetAngle(base.transform.position, base.transform.position + new Vector3(vx, vy));
                     state.LookAngle = state.facingAngle;
                     speed += (agent.speed - speed) / 3f * GameManager.DeltaTime;
 
-                    if (!isPlayerInRange)
-                        state.ChangeToIdleState();
                     break;
                 case StateMachine.State.HitLeft:
                 case StateMachine.State.HitRight:
                     detectionRange *= 2f;
                     break;
                 case StateMachine.State.Attacking:
-                    SpineTransform.localPosition = Vector3.zero;
-                    forceDir = state.facingAngle;
-                    AttackTimer += Time.deltaTime;
-                    distanceToPlayer = Vector3.Distance(transform.position, target.position);
+                    agent.speed = moveSpeed;
 
-                    if (AttackTimer >= AttackDuration / 2f && !hasAppliedDamage)
-                    {
-                    }
-                    else if (AttackTimer < AttackDuration / 2f)
-                    {
-                        hasAppliedDamage = false;
-                    }
+                    directionToTarget = (movePoint - transform.position).normalized;
 
-                    if (distanceToPlayer > AttackDistance && AttackTimer >= AttackDuration)
-                    {
-                        AttackTimer = 0f;
-                        state.CURRENT_STATE = StateMachine.State.Idle;
-                    }
-                    else if (AttackTimer < 0.1f)
-                    {
-                        state.facingAngle = (forceDir = Utils.GetAngle(Vector3.zero, new Vector3(xDir, yDir)));
-                    }
-                    else if (AttackTimer >= AttackDuration && AttackTimer < AttackDuration + AttackDelay)
-                    {
-                        state.CURRENT_STATE = StateMachine.State.Idle;
-                    }
-                    else if (AttackTimer >= AttackDuration + AttackDelay)
-                    {
-                        AttackTimer = 0f;
-                        state.CURRENT_STATE = StateMachine.State.Idle;
-                    }
-                    stopTimer = 0f;
-                    break;
-            }
-        }
-    }
+                    xDir = Mathf.Clamp(directionToTarget.x, -1f, 1f);
+                    yDir = Mathf.Clamp(directionToTarget.y, -1f, 1f);
 
-    private void FollowTarget()
-    {
-        if (state.CURRENT_STATE != StateMachine.State.Attacking)
-        {
-            float distanceToPlayer = Vector3.Distance(transform.position, target.position);
-
-            if (distanceToPlayer <= detectionRange)
-            {
-                isPlayerInRange = true;
-            }
-            else
-            {
-                isPlayerInRange = false;
-            }
-
-            if (isPlayerInRange && state.CURRENT_STATE != StateMachine.State.Attacking && stopTimer >= 1f)
-            {
-                Vector3 directionToTarget = (target.position - transform.position).normalized;
-
-                xDir = Mathf.Clamp(directionToTarget.x, -1f, 1f);
-                yDir = Mathf.Clamp(directionToTarget.y, -1f, 1f);
-
-                agent.SetDestination(target.position);
-                if (distanceToPlayer <= AttackDistance)
-                {
-                    state.CURRENT_STATE = StateMachine.State.Attacking;
-                    playerHealth.Damaged(gameObject, transform.position, Damaged);
-                    agent.isStopped = true;
-                }
-                else
-                {
+                    agent.SetDestination(movePoint);
                     agent.isStopped = false;
-                }
-            }
-            else
-            {
-                agent.isStopped = true;
-                xDir = 0f;
-                yDir = 0f;
+
+                    distanceToPoint = Vector3.Distance(transform.position, movePoint);
+                    if (distanceToPoint <= AttackDistance)
+                    {
+                        float stopTime = 0;
+                        agent.isStopped = true;
+                        state.CURRENT_STATE = StateMachine.State.Idle;
+                    }
+
+                    forceDir = Utils.GetAngle(Vector3.zero, new Vector3(xDir, yDir));
+                    state.facingAngle = Utils.GetAngle(base.transform.position, base.transform.position + new Vector3(vx, vy));
+                    state.LookAngle = state.facingAngle;
+                    speed += (agent.speed - speed) / 3f * GameManager.DeltaTime;
+
+                    break;
             }
         }
     }
@@ -219,4 +175,5 @@ public class CookieMouse : UnitObject
         Gizmos.color = Color.green;
         Gizmos.DrawWireSphere(transform.position, AttackDistance);
     }
+
 }
