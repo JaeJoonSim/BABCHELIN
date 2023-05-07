ea004108851fd447aaa031db3f99962d865b3fa2
Donghyun_lim <97100624+GwakDuMong@users.noreply.github.com>
2023-05-08 03:41:51 +0900

    add ChangeMaterial : Editor

---
+++ Assets/Editor/ChangeMaterial.cs
@@ -0,0 +1,24 @@
+#if UNITY_EDITOR
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class ChangeMaterial : Editor
+{
+    [MenuItem("Material/ChangeMaterial")]
+    public static void ChangeMat()
+    {
+        GameObject[] sprites = Selection.gameObjects;
+        foreach (GameObject sprite in sprites)
+        {
+            var s = sprite.GetComponent<SpriteRenderer>();
+            if (s != null)
+            {
+                var path = AssetDatabase.GUIDToAssetPath("72372d8c6b7d87247a37325d4825ce7a");
+                s.material = AssetDatabase.LoadAssetAtPath<Material>(path);
+                s.sortingOrder = 0;
+            }
+        }
+    }
+}
+#endif
