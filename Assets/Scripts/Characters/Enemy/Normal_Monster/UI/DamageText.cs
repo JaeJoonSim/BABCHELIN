1c9f930e95c9f66a56af54d5563d7e0c2c7d40ad
kdh990415 <kimdae990415@gmail.com>
2023-06-05 16:27:10 +0900

    데미지 폰트 서서히 삭제

--- Assets/Scripts/Characters/Enemy/Normal_Monster/UI/DamageText.cs
+++ Assets/Scripts/Characters/Enemy/Normal_Monster/UI/DamageText.cs
@@ -1,14 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class DamageText : MonoBehaviour
 {
     [SerializeField] float destroyTime;
     private float time;
+    private TMP_Text dmgText;
+    private Color textColor;
+
     // Start is called before the first frame update
     void Start()
     {
+        dmgText = GetComponent<TMP_Text>();
+        textColor = new Color(0, 0, 0, 1);
         time = 0;
     }
 
@@ -16,7 +23,9 @@ public class DamageText : MonoBehaviour
     void Update()
     {
         time += Time.deltaTime;
-        transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z - (time / 100));
+        transform.position = new Vector3(transform.position.x, transform.position.y + (time / 500), transform.position.z - (time / 100));
+        dmgText.color = textColor;
+        textColor.a -= (Time.deltaTime / destroyTime);
 
         if (time > destroyTime)
         {
