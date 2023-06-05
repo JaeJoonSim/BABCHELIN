ce2b32df78b1b98d1452436e8747c33443f77b69
kdh990415 <kimdae990415@gmail.com>
2023-06-05 13:52:28 +0900

    몬스터 체력바 fadein,out

--- Assets/Scripts/Characters/Enemy/Normal_Monster/UI/MonsterUI.cs
+++ Assets/Scripts/Characters/Enemy/Normal_Monster/UI/MonsterUI.cs
@@ -10,17 +10,23 @@ public class MonsterUI : MonoBehaviour
     private PlayerController playerController;
 
     public Image UIHealthGauge;
+    public Image UIHealthBackground;
+    private Color HPBarColor;
+    private bool fadeout;
+    private float time;
 
     // Start is called before the first frame update
     void Start()
     {
         monsterHealth = monster.GetComponent<Health>();
+        HPBarColor = new Color(1, 1, 1, 0);
     }
 
     // Update is called once per frame
     void Update()
     {
         GaugeManagement();
+        ShowHPBar();
 
         if (monster.localScale.x < 0)
         {
@@ -30,10 +36,38 @@ public class MonsterUI : MonoBehaviour
         {
             UIHealthGauge.gameObject.transform.localScale = new Vector3(0.004f, 0.004f, 0.004f);
         }
+
+        if (HPBarColor.a > 0)
+        {
+            FadeoutHPBar();
+        }
     }
 
     private void GaugeManagement()
     {
         UIHealthGauge.fillAmount = monsterHealth.CurrentHP() / monsterHealth.MaxHP();
+    }
+
+    private void ShowHPBar()
+    {
+        UIHealthGauge.color = HPBarColor;
+        UIHealthBackground.color = HPBarColor;
+
+        if (monsterHealth.CurrentHP() != monsterHealth.BackHP())
+        {
+            HPBarColor.a = 1;
+            time = 0;
+            fadeout = true;
+        }
+    }
+
+    private void FadeoutHPBar()
+    {
+        time += Time.deltaTime;
+
+        if(time >= 2)
+        {
+            HPBarColor.a -= Time.deltaTime * 2;
+        }
     }
 }
