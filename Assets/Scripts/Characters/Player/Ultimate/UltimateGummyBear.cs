a9e16c03923f150dcc7c5452b56e789ce146817f
DOEUNJI <wsxokm7654@gmail.com>
2023-06-07 18:58:32 +0900

    BasicRoom05 1

--- Assets/Scripts/Characters/Player/Ultimate/UltimateGummyBear.cs
+++ Assets/Scripts/Characters/Player/Ultimate/UltimateGummyBear.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class UltimateGummyBear : MonoBehaviour
 {
+    public float speed = .0f;
 
     private void Start()
     {
-        Destroy(gameObject, 6f);
+        Destroy(gameObject, speed);
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
