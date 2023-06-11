a92a0c771e9e07d18b2e9c7695c48e38d498f184
kdh990415 <kimdae990415@gmail.com>
2023-06-11 21:20:35 +0900

    메인메뉴 상호작용 구현

--- Assets/Scripts/Manager Scripts/MainMenuManager.cs
+++ Assets/Scripts/Manager Scripts/MainMenuManager.cs
@@ -13,6 +13,24 @@ public class MainMenuManager : MonoBehaviour
     [Header("Quit Button")]
     public Button QuitBtn;
 
+    [Space]
+    [Header("Key Setting")]
+    public KeyCode HideKey;
+    public KeyCode OptionKey;
+    public KeyCode CreditKey;
+    public KeyCode QuitKey;
+
+    public GameObject UICanvas;
+
+    [Space]
+    [Header("Option UI")]
+    private int tabNum;
+    public GameObject OptionUI;
+    public GameObject[] TabBtn;
+    public GameObject[] Tabs;
+
+    public GameObject CreditUI;
+
     private void Start()
     {
         if (StartBtn != null)
@@ -20,6 +38,17 @@ public class MainMenuManager : MonoBehaviour
 
         if (QuitBtn != null)
             QuitBtn.onClick.AddListener(() => QuitGame());
+
+        OptionUI.SetActive(false);
+        CreditUI.SetActive(false);
+    }
+
+    private void Update()
+    {
+        HideUI();
+        OpenOptionUI();
+        OpebCreditUI();
+        CloseGame();
     }
 
     public void LoadScene(string sceneName)
@@ -35,5 +64,61 @@ public class MainMenuManager : MonoBehaviour
 #else
         Application.Quit();
 #endif
+    }
+
+    private void HideUI()
+    {
+        if (Input.GetKeyDown(HideKey))
+        {
+            UICanvas.SetActive(!UICanvas.activeSelf);
+        }
+    }
+
+    private void OpenOptionUI()
+    {
+        if (Input.GetKeyDown(OptionKey))
+            OptionUI.SetActive(!OptionUI.activeSelf);
+
+        for (int a = 0; a < 5; a++)
+        {
+            if (tabNum == a)
+            {
+                Tabs[a].SetActive(true);
+                TabBtn[a].SetActive(false);
+            }
+            else
+            {
+                Tabs[a].SetActive(false);
+                TabBtn[a].SetActive(true);
+            }
+        }
+    }
+
+    public void SetTabNum(int a)
+    {
+        tabNum = a;
+    }
+
+    public void CloseButton()
+    {
+        OptionUI.SetActive(!OptionUI.activeSelf);
+    }
+
+    private void OpebCreditUI()
+    {
+        if (Input.GetKeyDown(CreditKey))
+            CreditUI.SetActive(!CreditUI.activeSelf);
+    }
+
+    private void CloseGame()
+    {
+        if (Input.GetKeyDown(QuitKey))
+        {
+#if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+        }
     }
 }
